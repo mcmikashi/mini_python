ceb69c27c89fb2cb7ac96d77dcaba7edaaceea96
mickael anicette <mickaelanicette@gmail.com>
2021-12-13 16:04:20 -0300

    Modification: module fibonaci ajout de la docstring a la fonction fibonacci

--- module/fibonaci.py
+++ module/fibonaci.py
@@ -2,6 +2,16 @@ from functools import cache
 
 @cache
 def fibonacci(n):
+    """ fibonacci
+    
+    This the rercusive function of the fibonacci suite
+    
+    Args:
+        n (int): the index of fibonacci
+
+    Returns:
+        [int]: the number of fibonacci at n index
+    """
     if n in [0,1]:
         return n
     return fibonacci(n-1) + fibonacci(n-2)
