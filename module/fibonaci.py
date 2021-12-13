58c265de796147a29d53a82407b5721a1584389e
mickael anicette <mickaelanicette@gmail.com>
2021-12-13 14:43:35 -0300

    Modification : module fibonacci

--- module/fibonaci.py
+++ module/fibonaci.py
@@ -1,2 +1,7 @@
-def fibonaci():
-    pass
+from functools import cache
+
+@cache
+def fibonacci(n):
+    if n in [0,1]:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
--- test/test_fibonaci.py
+++ test/test_fibonaci.py
@@ -6,29 +6,28 @@ from pathlib import Path
 # insert at 1, 0 is the script path (or '' in REPL)
 test_path = Path("module").absolute()
 
-print(test_path)
-
 sys.path.append(str(test_path))
 
 print(sys.path)
 
-from fibonaci import fibonaci
+from fibonaci import fibonacci
 
-class FibonaciTest(unittest.TestCase):
+class fibonacciTest(unittest.TestCase):
     
     def test_zeros_un_deux(self):
-        self.assertEqual(fibonaci(0),0)
-        self.assertEqual(fibonaci(1),1)
-        self.assertEqual(fibonaci(2),1)
+        self.assertEqual(fibonacci(0),0)
+        self.assertEqual(fibonacci(1),1)
+        self.assertEqual(fibonacci(2),1)
         
     def test_basic(self):
-        self.assertEqual(fibonaci(4),3)
-        self.assertEqual(fibonaci(8),21)
-        self.assertEqual(fibonaci(11),89)
-        self.assertEqual(fibonaci(16),987)
-        self.assertEqual(fibonaci(40),102334155)
-        self.assertEqual(fibonaci(64),10610209857723 )
-        self.assertEqual(fibonaci(94),19740274219868223167 )
-        self.assertEqual(fibonaci(200),280571172992510140037611932413038677189525 )
+        self.assertEqual(fibonacci(4),3)
+        self.assertEqual(fibonacci(8),21)
+        self.assertEqual(fibonacci(11),89)
+        self.assertEqual(fibonacci(16),987)
+        self.assertEqual(fibonacci(40),102334155)
+        self.assertEqual(fibonacci(64),10610209857723 )
+        self.assertEqual(fibonacci(94),19740274219868223167 )
+        self.assertEqual(fibonacci(200),280571172992510140037611932413038677189525 )
+
 if __name__ == '__main__':
     unittest.main()    
