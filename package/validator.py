3a1b0dc8bb217170ae909b9a818c6fe6e677141a
mickael anicette <mickaelanicette@gmail.com>
2021-12-14 11:15:09 -0300

    Modification et Ajout :  modification module deviens package, modification docstring de finobaci et  ajout du fichier validator

---
+++ package/validator.py
@@ -0,0 +1,20 @@
+def number_valid(chaine):
+    """Number Valid :
+    Determine if the text in the input is a number between 0 and 493
+
+    Args:
+        chaine (str): the input of the user
+
+    Returns:
+        [bool]: True if the input of the user is an integer between 0 and 493 else False
+    """
+    try:
+        if chaine=="":
+            return True
+        number = int(chaine)
+        if number in range(0,494):
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
