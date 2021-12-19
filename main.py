b918a8111d6a243a7c25616a22486b0b9309a287
mickael anicette <mickaelanicette@gmail.com>
2021-12-19 14:01:33 -0300

    Modification : main.py creation class MainInterface
    MainInterface class servant a generer l'interface

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from functools import wraps
 import tkinter as tk
 from tkinter import ttk
 from tkinter.constants import BOTH
@@ -8,168 +7,77 @@ from package.tribonacci import tribonacci
 from package.lucas import lucas
 from package.rabbit import dying_rabbit as rabbit
 
-# Intialisation des couleurs
-blue_dark = "#1d3557"
-blue_medium = "#457b9d"
-blue_ligth = "#a8dadc"
-blue_very_ligth = "#e0ffff"
+
+class MainInterface:
+    # Intialisation des couleurs
+    blue_dark = "#1d3557"
+    blue_ligth = "#a8dadc"
+    blue_very_ligth = "#e0ffff"
+
+    def __init__(self, root) -> None:
+        # Configuation de base
+        root.title = "Mini Python"
+        root.geometry("800x400")
+        # https://github.com/PapirusDevelopmentTeam/
+        root.iconbitmap("python.ico")
+        root.config(bg=self.blue_dark)
+        # Validateur input recursive
+        self.num_valid = root.register(number_valid)
+        # Creation du conteneur d'onglet
+        self.menu = ttk.Notebook(root)
+        self.menu.pack(fill=BOTH, expand=True)
+
+    def add_onglet(self, name, func):
+        # Creation de la frame de l'onglet
+        frame_generic = tk.LabelFrame(self.menu)
+        frame_generic.pack()
+        input_generic = tk.LabelFrame(frame_generic, bg=self.blue_ligth)
+        input_generic.pack(pady=10)
+        output_generic = tk.LabelFrame(frame_generic, bg=self.blue_ligth)
+        output_generic.pack(padx=10, pady=10, fill=BOTH, expand=True)
+
+        # fonction affichage generic
+        def affichage_generic():
+            nombre = index_generic.get()
+            index_generic.delete(0, "end")
+            text_index_choosen = f"index : {nombre}"
+            index_choosen.config(text=text_index_choosen,
+                                 bg=self.blue_very_ligth, font=25)
+            resultat = func(nombre)
+            resultat_generic.config(
+                text=resultat, bg=self.blue_very_ligth, font=25)
+
+        # button,input et label generic
+        label_index_generic = tk.Label(
+            input_generic, text="Entrez un index entre 0 et 493"
+        )
+        index_generic = tk.Entry(
+            input_generic, width=35, text="Quel est votre index ?",
+            validate="key", validatecommand=(self.num_valid, "%P")
+        )
+        submit_generic = tk.Button(
+            input_generic, text="Envoyer", command=affichage_generic
+        )
+
+        label_index_generic.grid(row=0, column=0, padx=10, pady=10)
+        index_generic.grid(row=0, column=1, padx=10, pady=10)
+        submit_generic.grid(row=0, column=2, padx=10, pady=10)
+        index_choosen = tk.Label(
+            output_generic, bg=self.blue_ligth, wraplength=500)
+        resultat_generic = tk.Label(
+            output_generic, bg=self.blue_ligth, wraplength=500)
+        index_choosen.pack(padx=10, pady=10)
+        resultat_generic.pack(padx=10, pady=10)
+        # Ajout de la frame au menu
+        self.menu.add(frame_generic, text=name)
+
 
 # Creation de la base graphique
-base = tk.Tk()
-base.title("Mini Python")
-base.geometry("800x400")
-base.iconbitmap("python.ico")  # https://github.com/PapirusDevelopmentTeam/
-base.config(bg=blue_dark)
+root = tk.Tk()
 
-#validateur input recursive
-num_valid = base.register(number_valid)
-
-""" les éléments de l'onglet fibonacci """
-
-# Creation du conteneur d'onglets
-menu = ttk.Notebook(base)
-menu.pack(fill=BOTH, expand=True)
-
-# Label Frame Fibonacci
-frame_fibonacci = tk.LabelFrame(menu)
-frame_fibonacci.pack()
-input_fibonacci = tk.LabelFrame(frame_fibonacci, bg=blue_medium)
-input_fibonacci.pack(pady=10)
-output_fibonacci = tk.LabelFrame(frame_fibonacci, bg=blue_ligth)
-output_fibonacci.pack(padx=10, pady=10, fill=BOTH, expand=True)
-
-# fonction affichage fibonacci
-def affichage_fibonacci():
-    nombre = index_fibonacci.get()
-    resultat = fibonacci(nombre)
-    resultat_fibonacci.config(text=resultat, bg=blue_very_ligth, font=25)
-
-
-# button,input et label fibonacci
-label_index_fibonacci = tk.Label(
-    input_fibonacci, text="Entrez un index entre 0 et 493"
-)
-index_fibonacci = tk.Entry(
-    input_fibonacci, width=35, text="Quel est votre index ?",
-    validate="key", validatecommand=(num_valid, "%P")
-)
-submit_fibonacci = tk.Button(
-    input_fibonacci, text="Envoyer", command=affichage_fibonacci
-)
-
-label_index_fibonacci.grid(row=0, column=0, padx=10, pady=10)
-index_fibonacci.grid(row=0, column=1, padx=10, pady=10)
-submit_fibonacci.grid(row=0, column=2, padx=10, pady=10)
-resultat_fibonacci = tk.Label(output_fibonacci, bg=blue_ligth, wraplength=500)
-resultat_fibonacci.pack(padx=10, pady=10)
-
-""" les éléments de l'onglet tribonacci """
-
-# Label Frame Tribonacci
-frame_tribonacci = tk.LabelFrame(menu)
-frame_tribonacci.pack()
-input_tribonacci = tk.LabelFrame(frame_tribonacci, bg=blue_medium)
-input_tribonacci.pack(pady=10)
-output_tribonacci = tk.LabelFrame(frame_tribonacci, bg=blue_ligth)
-output_tribonacci.pack(padx=10, pady=10, fill=BOTH, expand=True)
-
-
-# fonction affichage fibonacci
-def affichage_tribonacci():
-    nombre = index_tribonacci.get()
-    resultat = tribonacci(nombre)
-    resultat_tribonacci.config(text=resultat, bg=blue_very_ligth, font=25)
-
-# button,input et label Tribonacci
-label_index_tribonacci = tk.Label(
-    input_tribonacci, text="Entrez un index entre 0 et 493"
-)
-index_tribonacci = tk.Entry(
-    input_tribonacci, width=35, text="Quel est votre index ?",
-    validate="key", validatecommand=(num_valid, "%P")
-)
-submit_tribonacci = tk.Button(
-    input_tribonacci, text="Envoyer", command=affichage_tribonacci
-)
-label_index_tribonacci.grid(row=0, column=0, padx=10, pady=10)
-index_tribonacci.grid(row=0, column=1, padx=10, pady=10)
-submit_tribonacci.grid(row=0, column=2, padx=10, pady=10)
-resultat_tribonacci = tk.Label(output_tribonacci, bg=blue_ligth, wraplength=500)
-resultat_tribonacci.pack(padx=10, pady=10)
-
-""" les éléments de l'onglet lucas """
-
-# Label Frame lucas
-frame_lucas = tk.LabelFrame(menu)
-frame_lucas.pack()
-input_lucas = tk.LabelFrame(frame_lucas, bg=blue_medium)
-input_lucas.pack(pady=10)
-output_lucas = tk.LabelFrame(frame_lucas, bg=blue_ligth)
-output_lucas.pack(padx=10, pady=10, fill=BOTH, expand=True)
-
-
-# fonction affichage fibonacci
-def affichage_lucas():
-    nombre = index_lucas.get()
-    resultat = lucas(nombre)
-    resultat_lucas.config(text=resultat, bg=blue_very_ligth, font=25)
-
-# button,input et label lucas
-label_index_lucas = tk.Label(
-    input_lucas, text="Entrez un index entre 0 et 493"
-)
-index_lucas = tk.Entry(
-    input_lucas, width=35, text="Quel est votre index ?",
-    validate="key", validatecommand=(num_valid, "%P")
-)
-submit_lucas = tk.Button(
-    input_lucas, text="Envoyer", command=affichage_lucas
-)
-label_index_lucas.grid(row=0, column=0, padx=10, pady=10)
-index_lucas.grid(row=0, column=1, padx=10, pady=10)
-submit_lucas.grid(row=0, column=2, padx=10, pady=10)
-resultat_lucas = tk.Label(output_lucas, bg=blue_ligth, wraplength=500)
-resultat_lucas.pack(padx=10, pady=10)
-
-""" les éléments de l'onglet rabbit """
-
-# Label Frame rabbit
-frame_rabbit = tk.LabelFrame(menu)
-frame_rabbit.pack()
-input_rabbit = tk.LabelFrame(frame_rabbit, bg=blue_medium)
-input_rabbit.pack(pady=10)
-output_rabbit = tk.LabelFrame(frame_rabbit, bg=blue_ligth)
-output_rabbit.pack(padx=10, pady=10, fill=BOTH, expand=True)
-
-
-# fonction affichage fibonacci
-def affichage_rabbit():
-    nombre = index_rabbit.get()
-    resultat = rabbit(nombre)
-    resultat_rabbit.config(text=resultat, bg=blue_very_ligth, font=25)
-
-# button,input et label rabbit
-label_index_rabbit = tk.Label(
-    input_rabbit, text="Entrez un index entre 0 et 493"
-)
-index_rabbit = tk.Entry(
-    input_rabbit, width=35, text="Quel est votre index ?",
-    validate="key", validatecommand=(num_valid, "%P")
-)
-submit_rabbit = tk.Button(
-    input_rabbit, text="Envoyer", command=affichage_rabbit
-)
-label_index_rabbit.grid(row=0, column=0, padx=10, pady=10)
-index_rabbit.grid(row=0, column=1, padx=10, pady=10)
-submit_rabbit.grid(row=0, column=2, padx=10, pady=10)
-resultat_rabbit = tk.Label(output_rabbit, bg=blue_ligth, wraplength=500)
-resultat_rabbit.pack(padx=10, pady=10)
-""" les éléments du menu """
-
-# Ajout des onglets
-menu.add(frame_fibonacci, text="Fibonacci")
-menu.add(frame_tribonacci, text="Tribonacci")
-menu.add(frame_lucas, text="Lucas Numbers")
-menu.add(frame_rabbit, text="Dying rabbits")
-
-base.mainloop()
+gui = MainInterface(root)
+gui.add_onglet("fibonacci", fibonacci)
+gui.add_onglet("tribonacci", tribonacci)
+gui.add_onglet("lucas", lucas)
+gui.add_onglet("dying_rabbit", rabbit)
+root.mainloop()
